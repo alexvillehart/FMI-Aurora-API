4fd2934a30623b011c331927a6a8d52eb02f0d6f
Alex Haaja <posti@alexhaaja.fi>
2022-10-04 02:14:32 +0300

    Update comments.

--- parser.js
+++ parser.js
@@ -104,6 +104,7 @@ function getTimezoneOffsetInMlliseconds() {
     return new Date(Date.now()).getTimezoneOffset() * 60000
 }
 
+// Luo UTC aikaleiman muodossa DD-MM-YYYY HH:MM Z
 function UTCTimestamp() {
     let date = new Date(Date.now())
     let timestamp = ("0" + date.getUTCDate()).slice(-2) + "." + ("0" + date.getUTCMonth()).slice(-2) + "." + date.getUTCFullYear() + " " + ("0" + date.getUTCHours()).slice(-2) + ":" + ("0" + date.getUTCMinutes()).slice(-2) + ":" + ("0" + date.getUTCSeconds()).slice(-2) + "Z"
