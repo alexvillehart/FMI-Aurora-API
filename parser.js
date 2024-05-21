c21e31072510429e18960e7e7f9a1210fef8d72f
Alex Haaja <alex@aviate.fi>
2024-05-21 20:43:02 +0300

    Reworked parser, added caching, changed ratelimiter to 100 requests/h

--- parser.js
+++ parser.js
@@ -1,164 +1,56 @@
 import express, { request } from 'express'
 import axios from 'axios'
 import rateLimit from 'express-rate-limit'
+import cron from 'node-cron'
+import { stations } from './stations.js'
 
 // timestamp-arvon muuttamiseen.
 process.env['TZ'] = 'Europe/Helsinki'
 
 const app = express()
 const port = 3005
+const request_uri = 'https://cdn.fmi.fi/apps/magnetic-disturbance-observation-graphs/serve-data.php'
 
-/*
-    API-kyselyiden rajoittaminen 10 kyselyyn per 15min per IP-osoite.
-    Koska data päivittyy 10 minuutin välein, turha kysellä tietoja joka sekunti.
-    Eli effektiivinen raja on 40 kyselyä tunnissa. 
-*/
+
+// API-kyselyiden rajoittaminen 25 kyselyyn per 15min per IP-osoite. 100 kyselyä/tunti
+ 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minuuttia
-    max: 10,
+    max: 25,
     message: {'error': 'Too many requests, try again later.'},
     statusCode: 429,
     standardHeaders: true,
     legacyHeaders: false,
 })
 
-// Parametrit haettu FMI:n sivuilta 3.2.2024
-const request_uri = 'https://cdn.fmi.fi/apps/magnetic-disturbance-observation-graphs/serve-data.php'
-const stations = {
-    "KEV": {
-        "id": "KEV",
-        "threshold1": 55,
-        "threshold2": 165,
-        "names": {
-            "fi": "Kevo",
-            "en": "Kevo",
-            "sv": "Kevo"
-        }
-    },
-    "KIL": {
-        "id": "KIL",
-        "threshold1": 61,
-        "threshold2": 210,
-        "names": {
-            "fi": "Kilpisjärvi",
-            "en": "Kilpisjärvi",
-            "sv": "Kilpisjärvi"
-        }
-    },
-    "IVA": {
-        "id": "IVA",
-        "threshold1": 72,
-        "threshold2": 275,
-        "names": {
-            "fi": "Ivalo",
-            "en": "Ivalo",
-            "sv": "Ivalo"
-        }
-    },
-    "MUO": {
-        "id": "MUO",
-        "threshold1": 75,
-        "threshold2": 300,
-        "names": {
-            "fi": "Muonio",
-            "en": "Muonio",
-            "sv": "Muonio"
-        }
-    },
-    "SOD": {
-        "id": "SOD",
-        "threshold1": 74,
-        "threshold2": 290,
-        "names": {
-            "fi": "Sodankylä",
-            "en": "Sodankylä",
-            "sv": "Sodankylä"
-        }
-    },
-    "PEL": {
-        "id": "PEL",
-        "threshold1": 73,
-        "threshold2": 285,
-        "names": {
-            "fi": "Pello",
-            "en": "Pello",
-            "sv": "Pello"
-        }
-    },
-    "RAN": {
-        "id": "RAN",
-        "threshold1": 70,
-        "threshold2": 240,
-        "names": {
-            "fi": "Ranua",
-            "en": "Ranua",
-            "sv": "Ranua"
-        }
-    },
-    "OUJ": {
-        "id": "OUJ",
-        "threshold1": 68,
-        "threshold2": 200,
-        "names": {
-            "fi": "Oulujärvi",
-            "en": "Oulujärvi",
-            "sv": "Oulujärvi"
-        }
-    },
-    "MEK": {
-        "id": "MEK",
-        "threshold1": 64,
-        "threshold2": 150,
-        "names": {
-            "fi": "Mekrijärvi",
-            "en": "Mekrijärvi",
-            "sv": "Mekrijärvi"
-        }
-    },
-    "HAN": {
-        "id": "HAN",
-        "threshold1": 63,
-        "threshold2": 140,
-        "names": {
-            "fi": "Hankasalmi",
-            "en": "Hankasalmi",
-            "sv": "Hankasalmi"
-        }
-    },
-    "NUR": {
-        "id": "NUR",
-        "threshold1": 60,
-        "threshold2": 120,
-        "names": {
-            "fi": "Nurmijärvi",
-            "en": "Nurmijärvi",
-            "sv": "Nurmijärvi"
-        }
-    },
-    "TAR": {
-        "id": "TAR",
-        "threshold1": 55,
-        "threshold2": 100,
-        "names": {
-            "fi": "Tartto",
-            "en": "Tartu",
-            "sv": "Tartu"
-        }
-    }
-};
-
 // Proxyjen määrä
 app.set('trust proxy', 1)
 app.use(limiter)
 app.listen(port, () => {
-    console.log('[APP] FMI Parser is listening on port ' + port)
+    console.log(UTCTimestamp() + "\t[APP]\tStarted listening on port " + port)
+    getAllStationsLatestMeasurement().then(function(x) {
+        cache.data = x
+        cache.timestamp = UTCTimestamp()
+        console.log(UTCTimestamp() + "\t[CACHE]\tGenerated initial cache")
+    })
 })
-
-// Debugaamista varten
-app.get('/ip', (req, res) => res.send(req.ip))
 
 app.get('/', function(req, res) {
     res.status(404).send('Resources asked for not found.')
+})
+
+var cache = {
+    data: {},
+    timestamp: 0
+}
+
+cron.schedule('4,8,14,18,24,28,34,38,44,48,54,58 * * * *', () => {
+    console.log(UTCTimestamp() + " [CRON] Started cron job...")
+    getAllStationsLatestMeasurement().then(function(result) {
+        cache.data = result
+        cache.timestamp = Date.now()
+        console.log(UTCTimestamp() + " [CACHE] Renewed cached data")
+    })
 })
 
 app.get('/latest/:station/', function(req, res) {
@@ -166,8 +58,8 @@ app.get('/latest/:station/', function(req, res) {
     let validation = /\b([A-Za-z]{3})\b/g
     let station = req.params.station.toUpperCase()
     if(station.match(validation) && station in stations) {
-        console.log(UTCTimestamp() + " | " + req.ip + " [GET /latest/:station] User requested details for station: " + station)
-        return getLatestMeasurement(station).then((response) => {
+        console.log(UTCTimestamp() + "\t[GET /latest/:station]\t" + req.ip + "\tUser requested details for station: " + station)
+        return getLatestCachedMeasurement(station).then((response) => {
             res.send(response)
         })
         .catch((error) => {
@@ -176,52 +68,83 @@ app.get('/latest/:station/', function(req, res) {
             res.status(500).send(errorMsg)
         })
     } else {
-        console.log(UTCTimestamp() + " | " + req.ip + " [ERROR] User requested an unknown station or gave invalid input: " + station) 
+        console.log(UTCTimestamp() + "\t[ERROR]\t" + req.ip + "\tUser requested an unknown station or gave invalid input: " + station) 
         var errorMsg = {"error":"Invalid user input"}
         res.status(400).send(errorMsg)
     }
 })
 
-// Palauttaa viimeisimmän havainnon ja siihen liittyvät arvot
-/*
-    JSON muodossa
-    id: lyhytunniste (esim. NUR)
-    fi-name: Suomenkielinen nimi "Nurmijärvi"m
-    value: Mitattu magneettikentän arvo
-    threhsold: Raja-arvo jonka FMI on määrittänyt asemalle jolloin revontulien mahdollisuus on korkea
-    timestamp: Ihmisen luettavissa olettava kellonaika suomen aikaa.
-    timestamp_epoch: Epoch (ms) aikaleima UTC-aikaan
-    exceedsThreshold: boolean, true jos yrittää raja-arvon, false jos ei.
+
+
+async function getAllStationsLatestMeasurement() {
+    let measurements = {}
+    return await axios.get(request_uri)
+        .then(function(response) {
+            let data = response.data
+            for(const station in stations) {
+                let dataSerie = data[station].dataSeries
+                measurements[station] = parseMeasurement(station, dataSerie[dataSerie.length-1])
+            }
+            return measurements
+        })
+        .catch(function(error) {
+            console.log(UTCTimestamp() + "\t[ERROR]\tgetAllStationsLatestMeasurement encountered an error with the HTTP request: " + error)
+            return false
+        })
+}
+
+/* 
+    id: kolmikirjaiminen tunnus asemasta (esim. NUR)
+    names:                  Aseman selkokieliset nimet suomeksi, ruotsiksi ja englanniksi
+        {fi, sv, en}
+    value:                  Viimeisin mitattu R-arvo.
+    low_threshold:          FMI:n määrittelemä R-arvo jolloin revontulet ovat mahdollisia
+    high_threshold:         FMI:n määrittelemä R-arvo jolloin revontulet ovat todennäköisiä
+    exceeds_low_threshold:  true jos ylittää low_thresholdin, muuten false
+    exceeds_high_threshold: true jos ylittää high_thresholdin, muuten false
+    aurora_probability:     [none, low, high] sen mukaisesti, ylittyvätkö em. raja-arvot.
+    timestamp_fi:           selkokielinen aikaleima YYYY-MM-DD HH:MM:SS suomen aikaa.
+    epoch:                  UTC-aikaleima millisekunneissa. 
+
 */
 
-async function getLatestMeasurement(station) {
-    return await axios.get(request_uri)
-      .then(function(response) {
-        let measurement = response.data[station].dataSeries[response.data[station].dataSeries.length-1]
-        let timestamp = measurement[0] + getTimezoneOffsetInMlliseconds()
-        let humanTimestamp = new Date(measurement[0]).toISOString().replace(/T/, ' ').replace(/\..+/, '')
-        /* 
-            auroraProbability:
-            
-            Threshold1 exceeds => LOW
-            Threshold2 exceeds => HIGH
-            Neither happens => NONE
+function parseMeasurement(stationIdentifier, data) {
+    let auroraProbability
+    let timestamp           = data[0] + getTimezoneOffsetInMlliseconds()
+    let prettyTimestamp     = new Date(data[0]).toISOString().replace(/T/, ' ').replace(/\..+/, '')
+    let latestValue         = (data[1] === null) ? -1 : parseFloat(data[1].toFixed(2))
+    let station             = stations[stationIdentifier]
 
-        */
-        if(measurement[1] >= stations[station].threshold1 && measurement[1] <= stations[station].threshold2) {
-            var auroraProbability = "LOW"
-        } else if(measurement[1] >= stations[station].threshold1 && measurement[1] >= stations[station].threshold2) {
-            var auroraProbability = "HIGH"
-        } else {
-            var auroraProbability = "NONE"
-        }
-        let threshol1dAlert = (measurement[1] >= stations[station].threshold1) ? true : false
-        let threshold2Alert = (measurement[1] >= stations[station].threshold2) ? true : false
-        return {"id":station,"fi-name":stations[station].names.fi,"value":measurement[1],"threshold":stations[station].threshold,"timestamp":humanTimestamp,"timestamp_epoch":timestamp,"exceedsThreshold1": threshol1dAlert,"exceedsThreshold2": threshold2Alert, "auroraProbability": auroraProbability}
-      })
-      .catch(function(error) {
-        throw error
-      })
+
+    let exceeds_low_threshold = (latestValue >= station.low_threshold) ? true : false
+    let exceeds_high_threshold = (latestValue >= station.high_threshold) ? true: false
+
+
+    if(latestValue >= station.low_threshold && latestValue <= station.high_threshold) {
+        auroraProbability = 'low'
+    } else if(latestValue >= station.low_threshold && latestValue >= station.high_threshold) {
+        auroraProbability = 'high'
+    } else {
+        auroraProbability = 'none'
+    }
+
+    return {
+        "id": stationIdentifier,
+        "names": station.names,
+        "value": latestValue,
+        "low_threshold": station.low_threshold,
+        "high_threshold": station.high_threshold,
+        "exceeds_low_threshold": exceeds_low_threshold,
+        "exceeds_high_threshold": exceeds_high_threshold,
+        "aurora_probability": auroraProbability,
+        "timestamp_fi": prettyTimestamp,
+        "epoch":  timestamp
+    }
+
+}
+
+async function getLatestCachedMeasurement(station) {
+    return cache.data[station]
 }
 
 // Korjaa FMI:n bugin jossa epoch-aikaleima annetaan UTC muodossa mutta suomen aikaan.
@@ -235,3 +158,4 @@ function UTCTimestamp() {
     let timestamp = ("0" + date.getUTCDate()).slice(-2) + "." + ("0" + date.getUTCMonth()).slice(-2) + "." + date.getUTCFullYear() + " " + ("0" + date.getUTCHours()).slice(-2) + ":" + ("0" + date.getUTCMinutes()).slice(-2) + ":" + ("0" + date.getUTCSeconds()).slice(-2) + "Z"
     return timestamp
 }
+
---
+++ stations.js
@@ -0,0 +1,123 @@
+// Parametrit haettu FMI:n sivuilta 3.2.2024
+export const stations = {
+    "KEV": {
+        "id": "KEV",
+        "low_threshold": 55,
+        "high_threshold": 165,
+        "names": {
+            "fi": "Kevo",
+            "en": "Kevo",
+            "sv": "Kevo"
+        }
+    },
+    "KIL": {
+        "id": "KIL",
+        "low_threshold": 61,
+        "high_threshold": 210,
+        "names": {
+            "fi": "Kilpisjärvi",
+            "en": "Kilpisjärvi",
+            "sv": "Kilpisjärvi"
+        }
+    },
+    "IVA": {
+        "id": "IVA",
+        "low_threshold": 72,
+        "high_threshold": 275,
+        "names": {
+            "fi": "Ivalo",
+            "en": "Ivalo",
+            "sv": "Ivalo"
+        }
+    },
+    "MUO": {
+        "id": "MUO",
+        "low_threshold": 75,
+        "high_threshold": 300,
+        "names": {
+            "fi": "Muonio",
+            "en": "Muonio",
+            "sv": "Muonio"
+        }
+    },
+    "SOD": {
+        "id": "SOD",
+        "low_threshold": 74,
+        "high_threshold": 290,
+        "names": {
+            "fi": "Sodankylä",
+            "en": "Sodankylä",
+            "sv": "Sodankylä"
+        }
+    },
+    "PEL": {
+        "id": "PEL",
+        "low_threshold": 73,
+        "high_threshold": 285,
+        "names": {
+            "fi": "Pello",
+            "en": "Pello",
+            "sv": "Pello"
+        }
+    },
+    "RAN": {
+        "id": "RAN",
+        "low_threshold": 70,
+        "high_threshold": 240,
+        "names": {
+            "fi": "Ranua",
+            "en": "Ranua",
+            "sv": "Ranua"
+        }
+    },
+    "OUJ": {
+        "id": "OUJ",
+        "low_threshold": 68,
+        "high_threshold": 200,
+        "names": {
+            "fi": "Oulujärvi",
+            "en": "Oulujärvi",
+            "sv": "Oulujärvi"
+        }
+    },
+    "MEK": {
+        "id": "MEK",
+        "low_threshold": 64,
+        "high_threshold": 150,
+        "names": {
+            "fi": "Mekrijärvi",
+            "en": "Mekrijärvi",
+            "sv": "Mekrijärvi"
+        }
+    },
+    "HAN": {
+        "id": "HAN",
+        "low_threshold": 63,
+        "high_threshold": 140,
+        "names": {
+            "fi": "Hankasalmi",
+            "en": "Hankasalmi",
+            "sv": "Hankasalmi"
+        }
+    },
+    "NUR": {
+        "id": "NUR",
+        "low_threshold": 60,
+        "high_threshold": 120,
+        "names": {
+            "fi": "Nurmijärvi",
+            "en": "Nurmijärvi",
+            "sv": "Nurmijärvi"
+        }
+    },
+    "TAR": {
+        "id": "TAR",
+        "low_threshold": 55,
+        "high_threshold": 100,
+        "names": {
+            "fi": "Tartto",
+            "en": "Tartu",
+            "sv": "Tartu"
+        }
+    }
+}
